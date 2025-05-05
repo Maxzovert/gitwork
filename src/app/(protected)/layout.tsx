c6eb5e5d84176e3a12fa5aa061115124f300b499
Abdullah <95abdullah99@gmail.com>
2025-05-05 21:27:34 +0530

    feat: add SidebarLayout and dashboard components for user interface

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const SidebarLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SidebarLayout
