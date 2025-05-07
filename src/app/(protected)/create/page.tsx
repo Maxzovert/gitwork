c2ddd1aa09e8d0ad85422cc4729a078b6291727e
Abdullah <95abdullah99@gmail.com>
2025-05-07 21:12:06 +0530

    feat: add project creation functionality with authentication and notifications

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { api } from '@/trpc/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 type FormInput = {
     repoUrl: string
@@ -12,9 +14,21 @@ type FormInput = {
 
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
+    const createProject = api.project.createProtect.useMutation()
 
     function onSubmit(data : FormInput){
-        window.alert(JSON.stringify(data, null , 2))
+        createProject.mutate({
+            githubUrl: data.repoUrl,
+            name : data.projectName,
+            githubToken : data.githubToken
+        },{
+            onSuccess : () =>{
+                toast.success("Project Created sucessfully")
+            },
+            onError : () =>{
+                toast.error('Failed To create Project')
+            }
+        })
         return true
     }
   return (
